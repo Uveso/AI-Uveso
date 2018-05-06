7bed5947be0cb21401274ed4deec580b3e85546c
Uveso <Archivar@Zwergenteam.de>
2018-05-06 03:20:26 +0200

    Update V17

--- lua/AI/PlatoonTemplates/Uveso PlatoonTemplates Sea.lua
+++ lua/AI/PlatoonTemplates/Uveso PlatoonTemplates Sea.lua
@@ -3,28 +3,28 @@ PlatoonTemplate {
     Name = 'U123 Panic AntiSea 1 500',
     Plan = 'AttackPrioritizedSeaTargetsAIUveso',
     GlobalSquads = {
-        { categories.MOBILE * categories.NAVAL - categories.EXPERIMENTAL - categories.CARRIER - categories.NUKE, 1, 500, 'Attack', 'none' }
+        { categories.MOBILE * categories.NAVAL - categories.EXPERIMENTAL - categories.CARRIER - categories.NUKE, 1, 500, 'Attack', 'GrowthFormation' }
     },
 }
 PlatoonTemplate {
     Name = 'U123 Military AntiSea 5 5',
     Plan = 'AttackPrioritizedSeaTargetsAIUveso',
     GlobalSquads = {
-        { categories.MOBILE * categories.NAVAL - categories.EXPERIMENTAL - categories.CARRIER - categories.NUKE, 5, 5, 'Attack', 'none' }
+        { categories.MOBILE * categories.NAVAL - categories.EXPERIMENTAL - categories.CARRIER - categories.NUKE, 5, 5, 'Attack', 'GrowthFormation' }
     },
 }
 PlatoonTemplate {
     Name = 'U123 Enemy AntiSea 10 10',
     Plan = 'AttackPrioritizedSeaTargetsAIUveso',
     GlobalSquads = {
-        { categories.MOBILE * categories.NAVAL - categories.EXPERIMENTAL, 10, 10, 'Attack', 'none' }
+        { categories.MOBILE * categories.NAVAL - categories.EXPERIMENTAL, 10, 10, 'Attack', 'GrowthFormation' }
     }
 }
 PlatoonTemplate {
     Name = 'U123 KILLALL 10 10',
     Plan = 'AttackPrioritizedSeaTargetsAIUveso',
     GlobalSquads = {
-        { categories.MOBILE * categories.NAVAL, 10, 10, 'Attack', 'none' }
+        { categories.MOBILE * categories.NAVAL, 10, 10, 'Attack', 'GrowthFormation' }
     }
 }
 
