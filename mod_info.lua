451a39193042139e143685d5723978ce4f2f2557
Uveso <Archivar@Zwergenteam.de>
2020-01-14 23:53:29 +0100

    Fix V77

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "AI-Uveso"
-uid = "62e2j64a-AIUV-0760-89465-146as555a8u3"
-version = 76
+uid = "62e2j64a-AIUV-0770-89465-146as555a8u3"
+version = 77
 copyright = "2017-2020 Uveso"
 description = "(For FAF only)"
 author = "Uveso"
