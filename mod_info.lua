cfd8822d43a5f4a81d3b477592c7eb898042c641
Uveso <Archivar@Zwergenteam.de>
2024-12-13 21:17:15 +0100

    update version

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "AI-Uveso"
-uid = "62e2j64a-AIUV-113-89465-146as555a8u3"
-version = 113
+uid = "62e2j64a-AIUV-114-89465-146as555a8u3"
+version = 114
 copyright = "2017-2022 Uveso"
 description = "(For FAF only) Advanced turtle AI. Supports Nomads and other unit mods"
 author = "Uveso"
